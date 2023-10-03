ec46ae33fea487a045bd5fb466ce641cadc171aa
WRXer <wrxerrr@gmail.com>
2023-10-03 16:18:51 +0400

    настроил cors

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'users',
@@ -131,5 +132,10 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',  # Замените на адрес вашего фронтенд-сервера
+]
+CORS_ALLOW_ALL_ORIGINS = False
 
 
