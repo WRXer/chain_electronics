622a56f7828cc55a9625b7864ca7e239210247cd
WRXer <wrxerrr@gmail.com>
2023-10-03 16:42:41 +0400

    настроил документацию

--- config/settings.py
+++ config/settings.py
@@ -41,8 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'corsheaders',
     'rest_framework',
+    'drf_yasg',
 
     'users',
 ]
@@ -50,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
