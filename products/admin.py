3297aca21a6af031fc53d791f720449ffb81771f
WRXer <wrxerrr@gmail.com>
2023-10-04 16:30:55 +0400

    -добавил продукт в дамин панель -сделал автоматическое заполнение даты при создании продукта

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from products.models import Product
+
 
 # Register your models here.
+@admin.register(Product)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'model', 'release_date', 'is_active')
--- products/models.py
+++ products/models.py
@@ -8,8 +8,12 @@ class Product(models.Model):
     """
     name = models.CharField(max_length=50, verbose_name='название продукта')
     model = models.CharField(max_length=50, verbose_name='модель продукта')
-    release_date = models.DateField(verbose_name='дата выхода продукта')
+    release_date = models.DateField(auto_now_add=True, verbose_name='дата выхода продукта')
     is_active = models.BooleanField(default=True, verbose_name='активация продукта')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = 'продукты'
