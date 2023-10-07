7564f8adb79faf3db73d99cfc1838eaa5082906f
WRXer <wrxerrr@gmail.com>
2023-10-07 19:18:36 +0400

    настроил привязку автора к продукту в админке

--- products/admin.py
+++ products/admin.py
@@ -8,4 +8,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'model', 'release_date', 'manufacturer', 'is_active', 'author')
     exclude = ['author']
 
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user    #Если объект создается, а не редактируется, устанавливаем автора
+        super().save_model(request, obj, form, change)
+
 
--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название продукта')
     model = models.CharField(max_length=50, verbose_name='Модель продукта')
     release_date = models.DateField(auto_now_add=True, verbose_name='Дата выхода продукта')
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор', null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Автор')
     is_active = models.BooleanField(default=True, verbose_name='Активация продукта')
     manufacturer = models.ForeignKey(Partner, on_delete=models.CASCADE, limit_choices_to={'node_type': 'Завод'}, verbose_name='Организация производитель')
 
